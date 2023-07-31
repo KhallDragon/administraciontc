2b3c25af3fcef95742f23babeaf5cfd14cd75a22
Fernando Martini <fer.martini878@gmail.com>
2023-07-31 12:58:23 -0300

    hola

--- js/index.js
+++ js/index.js
@@ -18,21 +18,21 @@ btn.addEventListener('click', ()=>{
         <li class="border-bottom  border-success-subtle">Árqueos de caja. </li>
         <li class="border-bottom border-danger-subtle">Transferencias.</li>
         <li class="border-bottom  border-white">Emisión de echeq y cheques.</li>
-        <li class="border-bottom border-black">Conciliacion de cheques.</li>`],
+        <li class="border-bottom border-black">Conciliación de cheques.</li>`],
         typeSpeed: 10,
         startDelay: 500
       })
       const typed1 = new Typed('.escribir1', {
         strings : [ `<li class="border-bottom border-danger-subtle">Liquidación de iibb, iva. </li>
-        <li class="border-bottom border-white">Proyectacion de impuestos.</li>
+        <li class="border-bottom border-white">Proyectación de impuestos.</li>
         <li class="border-bottom border-black">Cancelación de veps. </li>`],
         typeSpeed: 10,
       })
       const typed2 = new Typed('.escribir2', {
-        strings : [ `<li class="border-bottom border-dark-subtle">Conciliacion ctas corrientes proveedores. </li>
-        <li class="border-bottom border-primary-subtle">Conciliacion ctas ctes clientes.</li>
+        strings : [ `<li class="border-bottom border-dark-subtle">Conciliación ctas corrientes proveedores. </li>
+        <li class="border-bottom border-primary-subtle">Conciliación ctas ctes clientes.</li>
         <li class="border-bottom border-success-subtle">Carga de comprobantes de compras. </li>
-        <li class="border-bottom border-danger-subtle">Emision. De comprobantes de ventas.  </li>
+        <li class="border-bottom border-danger-subtle">Emisión. De comprobantes de ventas.  </li>
         <li class="border-bottom border-white">Pagos de sueldos.</li>
         <li class="border-bottom border-black">Emisión de planillas de Excels. </li>`],
         typeSpeed: 10,
